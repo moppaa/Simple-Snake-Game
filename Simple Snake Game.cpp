b9c58831b5d8e60aef3a504cbca98922cebb53b6
moppa <daniejig@gmail.com>
2024-09-29 14:26:51 +0200

    1.02

--- Simple Snake Game.cpp
+++ Simple Snake Game.cpp
@@ -1,4 +1,5 @@
 ﻿#include <iostream>
+#include <conio.h>
 
 using namespace std;
 
@@ -23,7 +24,7 @@ void Setup()
 void Draw() 
 {
     system("cls");
-    for (int i = 0; i < WIDTH; i++)
+    for (int i = 0; i < WIDTH+1; i++)
     {
         cout << "#";
     }
@@ -35,7 +36,11 @@ void Draw()
         {
             if (j == 0)
                 cout << "#";
-            
+            if (i == y && j == x)
+                cout << "O";
+            else if (i == fruity && j == fruitx)
+                cout << "F";
+            else
                 cout << " ";
 
             if (j == WIDTH - 1)
@@ -44,25 +49,72 @@ void Draw()
         cout << endl;
     }
 
-    for (int i = 0; i < WIDTH; i++)
+    for (int i = 0; i < WIDTH+1; i++)
     {
         cout << "#";
     }
-    cout << endl;
+    cout << endl << endl;
+    cout << "Score: " << score << endl;
 }
 
 void Input()
 {
-
+    if (_kbhit())
+    {
+        switch (_getch())
+        {
+        case 'a':
+            dir = LEFT;
+            break;
+        case 'd':
+            dir = RIGHT;
+            break;
+        case 'w':
+            dir = UP;
+            break;
+        case 's':
+            dir = DOWN;
+            break;
+        case 'x':
+            gameOver = true;
+            break;
+        }
+    }
 }
 
 void Logic()
 {
+    switch (dir)
+    {
+    case LEFT:
+        x--;
+        break;
+    case RIGHT:
+        x++;
+        break;
+    case UP:
+        y--;
+        break;
+    case DOWN:
+        y++;
+        break;
+    default:
+        break;
+    }
 
+    if (x > WIDTH || x < 0 || y > HEIGHT || y < 0)
+        gameOver = true;
+    if (x == fruitx && y == fruity)
+    {
+        score += 10;
+        fruitx = rand() % WIDTH;
+        fruity = rand() % HEIGHT;
+    }
 }
 
 int main()
 {
+    srand(time(NULL));
     Setup();
     while (!gameOver)
     {
