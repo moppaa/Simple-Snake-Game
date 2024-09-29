193976ce937e9f6691a3cbef169ad34e83cc8fbb
moppa <daniejig@gmail.com>
2024-09-29 14:06:13 +0200

    1.01

--- Simple Snake Game.cpp
+++ Simple Snake Game.cpp
@@ -2,7 +2,75 @@
 
 using namespace std;
 
+bool gameOver;
+const int WIDTH = 20;
+const int HEIGHT = 20;
+int x, y, fruitx, fruity, score;
+enum eDirection {STOP = 0, LEFT, RIGHT, UP, DOWN};
+eDirection dir;
+
+void Setup()
+{
+    gameOver = false;
+    dir = STOP;
+    x = WIDTH / 2;
+    y = HEIGHT / 2;
+    fruitx = rand() % WIDTH;
+    fruity = rand() % HEIGHT;
+    score = 0;
+}
+
+void Draw() 
+{
+    system("cls");
+    for (int i = 0; i < WIDTH; i++)
+    {
+        cout << "#";
+    }
+    cout << endl;
+
+    for (int i = 0; i < HEIGHT; i++)
+    {
+        for (int j = 0; j < WIDTH; j++)
+        {
+            if (j == 0)
+                cout << "#";
+            
+                cout << " ";
+
+            if (j == WIDTH - 1)
+                cout << "#";
+        }
+        cout << endl;
+    }
+
+    for (int i = 0; i < WIDTH; i++)
+    {
+        cout << "#";
+    }
+    cout << endl;
+}
+
+void Input()
+{
+
+}
+
+void Logic()
+{
+
+}
+
 int main()
 {
+    Setup();
+    while (!gameOver)
+    {
+        Draw();
+        Input();
+        Logic();
+        //Sleep(10); sleep(10);
+    }
+
     return 0;
 }
